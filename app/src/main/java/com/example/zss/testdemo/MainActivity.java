c8f97503380f48df9afb1533adca47f27f94ace6
z-shasha <1968844105@qq.com>
2019-04-09 17:37:38 +0800

    添加代码

---
+++ app/src/main/java/com/example/zss/testdemo/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.zss.testdemo;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private TestFragment testFragment;
+    private Button btn;
+    private FrameLayout frameLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        frameLayout = (FrameLayout) findViewById(R.id.fraglayout);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn.setVisibility(View.GONE);
+                TestFragment testFragment = new TestFragment();
+                Bundle bundle = new Bundle();
+                bundle.putString("data", "5");
+                testFragment.setArguments(bundle);//数据传递到fragment中
+
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction transaction = fm.beginTransaction();
+                if (testFragment == null) {
+                    testFragment = new TestFragment();
+                }
+                transaction.replace(R.id.fraglayout, testFragment);
+                transaction.commit();
+//                transaction.detach(testFragment);
+
+            }
+        });
+
+        Intent intent = getIntent();
+        //从Intent当中根据key取得value
+        if (intent != null) {
+            String btn = intent.getStringExtra("text");
+        }
+    }
+}
---
+++ app/src/main/java/com/example/zss/testdemo/TestFragment.java
@@ -0,0 +1,45 @@
+package com.example.zss.testdemo;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TestFragment extends Fragment {
+    private TextView tv;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_test, container, false);
+        tv=(TextView)view.findViewById(R.id.tv1);
+        //界面传值5s
+        Bundle bundle =this.getArguments();//得到从Activity传来的数据
+        String mess = null;
+        if(bundle!=null){
+            mess = bundle.getString("data");
+        }
+        tv.setText(mess);
+        MyCountTimer myCountTimer = new MyCountTimer(6000, 1000, tv, "");
+        myCountTimer.start();
+        Intent intent=new Intent(getContext(),MainActivity.class);
+        intent.putExtra("text1","结束");
+        startActivity(intent);
+
+        return view;
+
+    }
+
+}
